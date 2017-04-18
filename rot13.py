1bc9703d42c06498f41b0624ebc68f728a5cc347
Steve Hanlon <rshanlon@gmail.com>
2017-04-18 10:13:25 -0700

    rot13.py update

--- rot13.py
+++ rot13.py
@@ -6,4 +6,30 @@ message = "Ohg, bssvpre, V qvqa'g pngpu gurfr -- gurl ner zl crg svfu naq V whfg
 """
 
 # Write your code here.
-3
+
+import string
+
+
+def decoder():
+    message = "Ohg, bssvpre, V qvqa'g pngpu gurfr -- gurl ner zl crg svfu naq V whfg\
+ oevat gurz urer gb fjvz. Jura gurl'er qbar gurl whzc onpx vagb gur ohpxrg.".lower()
+
+    punct = string.punctuation
+    space = string.whitespace
+
+    decoded = ''
+    for letters in message:
+        if letters in punct or letters in space:
+            decoded += letters
+        else:
+            if ord(letters) >= 110:
+                correct = ord(letters) - 13
+            elif ord(letters) <= 109:
+                correct = ord(letters) + 26 - 13
+            back_to_char = chr(correct)
+            decoded += back_to_char
+
+    return decoded
+
+
+decoder()

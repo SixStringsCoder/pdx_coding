11d39d3cdf79734c8310adef16b3b7be2ca18dbf
Steve Hanlon <rshanlon@gmail.com>
2017-04-23 22:53:02 -0700

    computer guessing Number Game computer guesses, using random int and binary method

---
+++ Binary_GuessingGame.py
@@ -0,0 +1,90 @@
+def binary_guess(guesses_allowed, low_num, high_num, correct_num):
+    """
+    computer makes based on binary search
+
+    :return: integer
+    """
+
+    num_guesses = 0
+
+    print("HAL, do you read me?")
+    print(f"Affirmative, Dan.")
+    print()
+    input(f"HAL, guess the number I'm thinking. It's between {low_num} and {high_num}.  >>>>> HIT RETURN")
+
+    comp_guess = int(high_num / 2)
+    print()
+    print(f'Is the number {comp_guess}, Dan?')
+
+    while guesses_allowed != 0:
+        guesses_allowed -= 1
+        num_guesses += 1
+
+        if comp_guess < correct_num:  # TOO LOW
+            measure = input(f'Too low, HAL. Guess higher. --------------{num_guesses} guess---{low_num} low---{high_num} high')
+            low_num += high_num - comp_guess
+            comp_guess = int((high_num - low_num) / 2 + low_num)
+
+        elif comp_guess > correct_num:  # TOO HIGH
+            measure = input(f'Too high, HAL. Guess lower. --------------{num_guesses} guess---{low_num} low---{high_num} high')
+            high_num -= comp_guess + low_num
+            comp_guess = int((high_num - low_num) / 2 + low_num)
+
+        elif comp_guess == correct_num:  # CORRECT
+            print(f'You got it, HAL! The number is {correct_num}!  You guessed it in {num_guesses} tries.')
+            break
+
+        print(f'Is the number {comp_guess}, Dan? ------------{low_num} low---{high_num} high')
+
+    print(f'Sorry, HAL! The number is {correct_num}!')
+
+
+binary_guess(16, 1, 1000, 280)
+
+
+
+
+#
+# import random
+#
+#
+# def binary_guess(guesses_allowed, low_num, high_num, correct_num):
+#     """
+#     computer makes random guesses but does not strategize based on passed mistakes
+#
+#     :return: integer
+#     """
+#
+#     num_guesses = 0
+#     print("HAL, do you read me?")
+#     print(f"Affirmative, Dan.")
+#     print()
+#     input(f"HAL, guess the number I'm thinking. It's between {low_num} and {high_num}.  >>>>> HIT RETURN")
+#     comp_guess = random.randint(low_num, high_num)
+#     print()
+#     print(f"Is the number {comp_guess}, Dan?")
+#
+#     while guesses_allowed != 0:
+#         guesses_allowed -= 1
+#         num_guesses += 1
+#
+#
+#         if comp_guess < correct_num:                                                                    #TOO LOW
+#             measure = input(f'Too low, HAL. Guess higher. {guesses_allowed}--{num_guesses}')
+#             comp_guess = random.randint(comp_guess, high_num)
+#             # print(f"Is the number {comp_guess}, Dan?")
+#
+#         elif comp_guess > correct_num:                                                                  #TOO HIGH
+#             measure = input(f'Too high, HAL. Guess lower. {guesses_allowed}--{num_guesses}')
+#             comp_guess = random.randint(low_num, comp_guess)
+#             # print(f"Is the number {comp_guess}, Dan?")
+#
+#         elif comp_guess == correct_num:                                                                 #CORRECT
+#             print(f"You got it, HAL! The number is {correct_num}!  You guess it in {num_guesses}.")
+#
+#         print(f"Is the number {comp_guess}, Dan?")
+#
+#     print(f"Sorry, HAL! The number is {correct_num}!")
+#
+#
+# binary_guess(16, 1, 1000, 373)

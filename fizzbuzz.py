f31f3507669a60c6775a94d13e6f66d839ca0169
Steve Hanlon <rshanlon@gmail.com>
2017-04-25 15:05:22 -0700

    added another way to FizzBuzz with just IFs

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -30,7 +30,7 @@ def fizz_buzz(n):
             fizzy_lister.append(str(c))
         c += 1
 
-    print(fizzy_lister)
+    return fizzy_lister
 
 
 def joined_buzz(n):
@@ -56,6 +56,33 @@ def joined_buzz(n):
 
     return str(string0rama)
 
+
+# def joined_buzz(n):
+#     """
+#     FizzBuzz using IFs and no ELIFs
+#     :param n:
+#     :return: string
+#     """
+#     #     fizzy_lister = list()
+#     c = 1
+#
+#     while c <= n:
+#         item = str()
+#         if c % 3 == 0:
+#             item += 'Fizz'
+#         if c % 5 == 0:
+#             item += 'Buzz'
+#         else:
+#             item = str(c)
+#
+#         fizzy_lister.append(item)
+#
+#         c += 1
+#
+#     string0rama = ' '.join(fizzy_lister)
+#
+#     # print("'" + string0rama + "'")
+#     return string0rama
 
 
 # def joined_buzz(n):

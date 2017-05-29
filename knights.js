d34140cf888e9977e4c528c59677f275f9bab613
Steve Hanlon <rshanlon@gmail.com>
2017-05-28 22:50:51 -0700

    first lab in JS using old Python labs

---
+++ knights.js
@@ -0,0 +1,10 @@
+/*
+ Created by Steve Hanlon.  Taking Python labs and changing to Javascript
+ */
+
+
+function knights() {
+    return "The knights who say Ni secretly try out Javascript and shout Nay!"
+}
+
+knights();

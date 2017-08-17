a45f63a0b455c4105c9d1ea818be8241054182c4
SixStringsCoder <rshanlon@gmail.com>
2017-08-16 22:37:01 -0700

    initial view of json to come from company

---
+++ json_parse.js
@@ -0,0 +1,38 @@
+<script>
+
+var data = [{ 'stats_code': 'active',
+	'alias': 'S510 Qintessence Shaw 7/2',
+	'mesh_id': 'C2D9BEA3',
+	'activator': 'alan.nwv@gmail.com',
+	'last_data_time': '2017-08-10T05:04:20+00:00',
+	'id': 156,
+	'activated_on': '2015-05-07T18:20:24+00:00'},
+
+	{'stats_code': 'active',
+	'alias': 'S511 Colmbia Crest 5/14 WS',
+	'mesh_id': 'C2F08264',
+	'activator': 'alan.nwv@gmail.com',
+	'last_data_time': '2017-08-10T03:07:11+00:00',
+	'id': 157,
+	'activated_on': '2015-05-08T17:24:31+00:00'
+}];
+
+data[0]
+
+
+
+
+for(var i = 0, i <= data.length; i++;){
+    data.splice(i, data.length);
+    alert(data)
+}
+
+// var extracted= data.splice(0,1)
+// alert(extracted)
+
+
+// var json = JSON.parse(extracted);
+// alert(json.status_code); //active
+
+
+</script>
